7a2cf81249fbfd845d1de71a1a1330c857280d99
jeremyscatigna <scatignaj@me.com>
2019-05-15 14:28:17 +0200

    init

---
+++ cmake-build-debug/CMakeFiles/2DHistogramFilterCPP.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/2DHistogramFilterCPP.dir/tests.cpp.o"
+  "2DHistogramFilterCPP.pdb"
+  "2DHistogramFilterCPP"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/2DHistogramFilterCPP.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
